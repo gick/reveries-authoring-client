4737df2be24e70bb3dc6d8a9a41140de514c8543
gick <gicquel.pierreyves@gmail.com>
2018-03-16 11:33:22 +0100

    perf

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
-const PolymerProject = require('polymer-build').PolymerProject;
+const {generateCountingSharedBundleUrlMapper,
+  generateSharedDepsMergeStrategy} = require('polymer-bundler');
+  const gulp = require('gulp');
+  const mergeStream = require('merge-stream');
+  const PolymerProject = require('polymer-build').PolymerProject;
 
 const project = new PolymerProject(require('./polymer.json'));
 
-const gulp = require('gulp');
-const mergeStream = require('merge-stream');
-
-// Create a build pipeline to pipe both streams together to the 'build/' dir
+  
 mergeStream(project.sources(), project.dependencies())
+  .pipe(project.bundler({
+    excludes: ['bower_components/polymer-code-mirror'],
+    sourcemaps: true,
+    stripComments: true,
+    strategy: generateSharedDepsMergeStrategy(3),
+    urlMapper: generateCountingSharedBundleUrlMapper('shared/bundle_')
+  }))
   .pipe(gulp.dest('build/'));
